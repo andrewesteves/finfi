694bfa8c628b533fe3a799540cc10842f997f9df
andrewesteves <esteves.andrew@gmail.com>
2019-11-03 13:12:54 -0300

    Income controller actions and mux router

--- controller/income_controller.go
+++ controller/income_controller.go
@@ -2,19 +2,93 @@ package controller
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/andrewesteves/finfi/model"
+	"github.com/gorilla/mux"
 )
 
 type IncomeController struct{}
 
-func (i IncomeController) Routes() http.HandlerFunc {
+func (i IncomeController) Index() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			incomes := model.IncomeModel{}
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(incomes.All())
+		incomes := model.IncomeModel{}
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(incomes.All())
+	}
+}
+
+func (i IncomeController) Show() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+		income := model.IncomeModel{
+			Title: fmt.Sprintf("Item %v", id),
+			Client: model.ClientModel{
+				Name:  "Bill Gates",
+				Email: "bill@microsoft.com",
+			},
+			Description:  "Lorem ipsum...",
+			Status:       "Paid",
+			Installments: 0,
+			Total:        100.00,
 		}
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(income)
+	}
+}
+
+func (i IncomeController) Store() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var income model.IncomeModel
+		reqBody, _ := ioutil.ReadAll(r.Body)
+		json.Unmarshal(reqBody, &income)
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(income)
+	}
+}
+
+func (i IncomeController) Update() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+		income := model.IncomeModel{
+			Title: fmt.Sprintf("Item %v", id),
+			Client: model.ClientModel{
+				Name:  "Bill Gates",
+				Email: "bill@microsoft.com",
+			},
+			Description:  "Lorem ipsum...",
+			Status:       "Paid",
+			Installments: 0,
+			Total:        100.00,
+		}
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(income)
+	}
+}
+
+func (i IncomeController) Destroy() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+		income := model.IncomeModel{
+			Title: fmt.Sprintf("Item %v", id),
+			Client: model.ClientModel{
+				Name:  "Bill Gates",
+				Email: "bill@microsoft.com",
+			},
+			Description:  "Lorem ipsum...",
+			Status:       "Paid",
+			Installments: 0,
+			Total:        100.00,
+		}
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(income)
 	}
 }
--- router.go
+++ router.go
@@ -4,17 +4,22 @@ import (
 	"net/http"
 
 	"github.com/andrewesteves/finfi/controller"
+	"github.com/gorilla/mux"
 )
 
-func Serve() *http.ServeMux {
+func Serve() *mux.Router {
 	var user controller.UserController
 	var client controller.ClientController
 	var income controller.IncomeController
 
-	mux := http.NewServeMux()
+	mux := mux.NewRouter().StrictSlash(true)
 	mux.HandleFunc("/users", user.Routes())
 	mux.HandleFunc("/clients", client.Routes())
-	mux.HandleFunc("/incomes", income.Routes())
+	mux.HandleFunc("/incomes", income.Index()).Methods("GET")
+	mux.HandleFunc("/incomes", income.Store()).Methods("POST")
+	mux.HandleFunc("/incomes/{id}", income.Show()).Methods("GET")
+	mux.HandleFunc("/incomes/{id}", income.Update()).Methods("PUT")
+	mux.HandleFunc("/incomes/{id}", income.Destroy()).Methods("DELETE")
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world"))
