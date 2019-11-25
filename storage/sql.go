38d3b52b03b0715b7244c8baee1ed1a0861638f0
andrewesteves <esteves.andrew@gmail.com>
2019-11-25 13:02:01 -0300

    Const db settings

--- storage/sql.go
+++ storage/sql.go
@@ -2,12 +2,18 @@ package storage
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+const (
+	user = "root"
+	pass = "4321"
+)
+
 func Connection() *sql.DB {
-	db, err := sql.Open("mysql", "root:4321@tcp(127.0.0.1:3306)/finfi?parseTime=true")
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/finfi?parseTime=true", user, pass))
 	if err != nil {
 		panic(err.Error())
 	}
